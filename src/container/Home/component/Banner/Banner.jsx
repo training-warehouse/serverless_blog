b48ec0c259b6466231c4c0146d9f9e20006e5540
LiaoKong <568250549@qq.com>
2022-04-26 22:53:49 +0800

    完成 banner组件

--- src/container/Home/component/Banner/Banner.jsx
+++ src/container/Home/component/Banner/Banner.jsx
@@ -1,6 +1,14 @@
+import styles from './style.module.scss'
+
 const Banner = () => {
     return (
-        <div>Banner</div>
+        <div className={styles.banner}>
+            <div className={styles.person}>
+                <img className={styles.avatar} src='/images/avatar.jpeg' alt=""/>
+                <div className={styles.title}>了空的个人小站</div>
+                <div className={styles.description}>从编程到生活，分享每天的点滴收获。</div>
+            </div>
+        </div>
     )
 }
 
---
+++ src/container/Home/component/Courses/index.jsx
@@ -0,0 +1,63 @@
+import styles from './style.module.scss'
+
+const Courses = () => {
+    return (
+        <div className={styles.courses}>
+            <ul className={styles.list}>
+                <li className={styles.item}>
+                    <img className={styles.img} src="https://eux-public.bj.bcebos.com/2018/07/02/df233513dd3e36bafdd4a15a69415bdc.jpg" alt=""/>
+                    <h4 className={styles.title}>Course</h4>
+                    <p className={styles.description}>从“写代码”到“写好代码”到“设计代码”，不仅是技术的提升，更是编程思维的提升，其中最关键的就是设计模式。但很多人想学习设计模式时，往往是查到的资料一堆，有用的知识少见，学得云里雾里，难以实践。本课从23种设计模式中精选前端常用的7种设计模式，利用场景化实例教学，让想学的人真正学明白、会应用、能实践。</p>
+                </li>
+                                <li className={styles.item}>
+                    <img className={styles.img} src="https://img3.mukewang.com/5aaf5f370001c40306000338-240-180.jpg" alt=""/>
+                    <h4 className={styles.title}>Course</h4>
+                    <p className={styles.description}>Course description</p>
+                </li>
+                                                <li className={styles.item}>
+                    <img className={styles.img} src="https://img3.mukewang.com/5aaf5f370001c40306000338-240-180.jpg" alt=""/>
+                    <h4 className={styles.title}>Course</h4>
+                    <p className={styles.description}>Course description</p>
+                </li>
+                                                                <li className={styles.item}>
+                    <img className={styles.img} src="https://img3.mukewang.com/5aaf5f370001c40306000338-240-180.jpg" alt=""/>
+                    <h4 className={styles.title}>Course</h4>
+                    <p className={styles.description}>Course description</p>
+                </li>
+                                                                <li className={styles.item}>
+                    <img className={styles.img} src="https://img3.mukewang.com/5aaf5f370001c40306000338-240-180.jpg" alt=""/>
+                    <h4 className={styles.title}>Course</h4>
+                    <p className={styles.description}>Course description</p>
+                </li>
+                                                                <li className={styles.item}>
+                    <img className={styles.img} src="https://img3.mukewang.com/5aaf5f370001c40306000338-240-180.jpg" alt=""/>
+                    <h4 className={styles.title}>Course</h4>
+                    <p className={styles.description}>Course description</p>
+                </li>
+                                                                <li className={styles.item}>
+                    <img className={styles.img} src="https://img3.mukewang.com/5aaf5f370001c40306000338-240-180.jpg" alt=""/>
+                    <h4 className={styles.title}>Course</h4>
+                    <p className={styles.description}>Course description</p>
+                </li>
+                                                                <li className={styles.item}>
+                    <img className={styles.img} src="https://img3.mukewang.com/5aaf5f370001c40306000338-240-180.jpg" alt=""/>
+                    <h4 className={styles.title}>Course</h4>
+                    <p className={styles.description}>Course description</p>
+                </li>
+                                                                <li className={styles.item}>
+                    <img className={styles.img} src="https://img3.mukewang.com/5aaf5f370001c40306000338-240-180.jpg" alt=""/>
+                    <h4 className={styles.title}>Course</h4>
+                    <p className={styles.description}>Course description</p>
+                </li>
+                                                                <li className={styles.item}>
+                    <img className={styles.img} src="https://img3.mukewang.com/5aaf5f370001c40306000338-240-180.jpg" alt=""/>
+                    <h4 className={styles.title}>Course</h4>
+                    <p className={styles.description}>Course description</p>
+                </li>
+
+            </ul>
+        </div>
+    )
+}
+
+export default Courses
